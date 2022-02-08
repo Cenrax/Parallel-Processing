5ab0b69b3d819d4ede90e0ee593ae7b28a069a93
Subham Kundu <43017632+Cenrax@users.noreply.github.com>
2022-02-08 16:39:42 +0530

    Update divideIntoChunks.py

--- parallelData/divideIntoChunks.py
+++ parallelData/divideIntoChunks.py
@@ -7,3 +7,14 @@ def make_chunks(df, num_chunks):
     return chunks
 
 #skill_chunks = make_chunks(skills,8)
+
+import concurrent.futures
+
+skill_chunks = make_chunks(skills,8)
+
+with concurrent.futures.ProcessPoolExecutor() as executor:
+    futures = [executor.submit(count_skills, job_postings,skill_chunk) for skill_chunk in skill_chunks]
+    
+results = [future.result() for future in futures]
+
+print(results[0])
